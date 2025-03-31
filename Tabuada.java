c4a2f5b4c629ede366acc026f4bd73b2873ea762
Kakkaswlz <kauaprestes1a@gmail.com>
2025-03-31 12:09:03 -0300

    Tabuada do número escolhido

---
+++ Tabuada.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Tabuada
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner(System.in);
+	    int n;
+	    
+	    System.out.println("Insira a tabuada que deseja ver: ");
+	    n = ler.nextInt();
+	    
+	    for(int i=1; i <= 10; i++){
+	    System.out.println(n + " x " + i + " = " + (n * i));
+	    }
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	}
+}
