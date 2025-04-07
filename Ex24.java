bbb204fc015d872b4ed66832d9cfcba7369af2a6
Kakkaswlz <kauaprestes1a@gmail.com>
2025-04-07 10:44:16 -0300

    Ex24 Soma dos números de 1 a 10

---
+++ Ex24.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+Soma dos números de 1 a 10 =...
+*******************************************************************************/
+public class Ex24
+{
+	public static void main(String[] args) {
+	    int soma = 0;
+	    int numero = 1;
+	    while (numero <= 10) {
+	        soma += numero;
+	        numero ++;}
+		System.out.println("A soma dos números de 1 a 10 é " + soma);
+	}
+}
